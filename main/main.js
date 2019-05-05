668d718f49f341e211b67230f823e0ce65c71351
dehmer <horst.dehmer@gmail.com>
2019-05-05 10:48:45 +0200

    moved main process entry point to main/ directory.

--- main/main.js
+++ main/main.js
@@ -23,8 +23,8 @@ const createWindow = () => {
     // NOTE: If browser complains about 'Not allowed to load local resource',
     //       the file is probable not there.
 
-    console.log('__dirname', __dirname)
-    console.log('app.getAppPath()', app.getAppPath())
+    console.log('__dirname', __dirname) // directory of main.js, i.e. directory name of the current module
+    console.log('app.getAppPath()', app.getAppPath()) // app.asar/
 
     // Note: Once packaged current working directory is ASARs root directory.
     window.loadFile('src/index.html')
